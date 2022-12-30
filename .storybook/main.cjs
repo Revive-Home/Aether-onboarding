3aead50fc35345344f8fcabc34cb2fee3804b69c
hoshikitsunoda <hoshkitsunoda@gmail.com>
2022-12-30 14:06:05 -0800

    TECH-5823 - Installed Storybook

---
+++ .storybook/main.cjs
@@ -0,0 +1,31 @@
+module.exports = {
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+  ],
+  framework: "@storybook/react",
+  core: {
+    builder: "@storybook/builder-vite",
+  },
+  async viteFinal(config) {
+    return {
+      ...config,
+      esbuild: {
+        ...config.esbuild,
+        jsxInject: `import React from 'react'`,
+      },
+      rollupOptions: {
+        ...config.rollupOptions,
+        external: ["react", "react-dom"],
+        output: {
+          globals: {
+            react: "React",
+            "react-dom": "ReactDOM",
+          },
+        },
+      },
+    };
+  },
+};
