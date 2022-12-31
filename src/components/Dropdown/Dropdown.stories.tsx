73b0d3664b01205b909d933784dbd0382ad5f1a8
hoshikitsunoda <hoshkitsunoda@gmail.com>
2022-12-30 21:10:45 -0800

    TECH-5823 - Refactored Dropdown

--- src/components/Dropdown/Dropdown.stories.tsx
+++ src/components/Dropdown/Dropdown.stories.tsx
@@ -7,9 +7,18 @@ export default {
   component: Dropdown,
 } as ComponentMeta<typeof Dropdown>;
 
+const links = [
+  { href: "/account-settings", label: "Account settings" },
+  { href: "/support", label: "Support" },
+  { href: "/license", label: "License" },
+  { href: "/sign-out", label: "Sign out" },
+];
+
 const Template: ComponentStory<typeof Dropdown> = (args) => (
   <Dropdown {...args} />
 );
 
 export const Primary = Template.bind({});
-Primary.args = {};
+Primary.args = {
+  list: links,
+};
--- src/components/Dropdown/Dropdown.tsx
+++ src/components/Dropdown/Dropdown.tsx
@@ -1,30 +1,25 @@
 import { Fragment } from "react";
 import { Menu } from "@headlessui/react";
 
-export interface DropdownProps {}
+export interface DropdownProps {
+  list: Record<string, any>[];
+}
 
-const links = [
-  { href: "/account-settings", label: "Account settings" },
-  { href: "/support", label: "Support" },
-  { href: "/license", label: "License" },
-  { href: "/sign-out", label: "Sign out" },
-];
-
-const Dropdown = ({}: DropdownProps): JSX.Element => {
+const Dropdown = ({ list }: DropdownProps): JSX.Element => {
   return (
     <Menu>
       <Menu.Button className="bg-white font-bold">Options</Menu.Button>
       <Menu.Items>
-        {links.map((link) => (
-          <Menu.Item key={link.href} as={Fragment}>
+        {list.map((item) => (
+          <Menu.Item key={item.href} as={Fragment}>
             {({ active }) => (
               <a
-                href={link.href}
+                href={item.href}
                 className={`${
                   active ? "bg-blue-500 text-white" : "bg-white text-black"
                 }`}
               >
-                {link.label}
+                {item.label}
               </a>
             )}
           </Menu.Item>
