73b3322e3ae327a64f70f30a25a53a3df56634a9
hoshikitsunoda <hoshkitsunoda@gmail.com>
2022-12-30 21:55:44 -0800

    TECH-5823 - Added Vitest and RTL

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
 import react from "@vitejs/plugin-react";
@@ -33,5 +34,10 @@ export default defineConfig({
     alias: {
       "@": resolve(__dirname, "src"),
     },
+  },
+  test: {
+    environment: "jsdom",
+    globals: true,
+    setupFiles: "./tests/setup.js",
   },
 });
