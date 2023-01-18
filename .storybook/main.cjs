d6daa54f2a2776217e0b73a2a474d1a7fab9cb44
hoshikitsunoda <hoshkitsunoda@gmail.com>
2023-01-17 23:55:52 -0800

    TECH-6035 - Added Storybook a11y

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -3,9 +3,10 @@ const { resolve } = require("path");
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
-    "@storybook/addon-links",
+    "@storybook/addon-a11y",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
+    "@storybook/addon-links",
   ],
   framework: "@storybook/react",
   core: {
