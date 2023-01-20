8f6815c2bf10d035c4a3c01c81e2ab2e56210866
hoshikitsunoda <hoshkitsunoda@gmail.com>
2023-01-20 10:31:06 -0800

    TECH-6035 - Added prop description

--- src/components/Typography/Typography.tsx
+++ src/components/Typography/Typography.tsx
@@ -8,10 +8,15 @@ import {
 import { getTag } from "../../../utils";
 
 export interface TypographyProps {
+  /** Optional HTML tag used for the root node. */
   as?: TagNameProps;
+  /** Optional Tailwind class names to override or extend the styles applied to the component. */
   className?: string;
+  /** The content of the component. */
   children: React.ReactNode;
+  /** Applies the theme typography styles. */
   variant: VariantProps;
+  /** Optional attribute applies different font weight */
   weight?: "medium" | "semibold" | "bold" | "default";
 }
 
