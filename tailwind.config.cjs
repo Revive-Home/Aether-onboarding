5d4b0c913fb48efa0c34aa2301a5aa1e6d583388
hoshikitsunoda <hoshkitsunoda@gmail.com>
2023-01-19 23:43:59 -0800

    TECH-6035 - Added a comment

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   mode: "jit",
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
     fontFamily: {
+      // `sans` is the default font in Tailwind. Overriding it will change the default font.
       sans: ["Poppins", "sans-serif"],
       nunito: ["Nunito", "sans-serif"],
     },
