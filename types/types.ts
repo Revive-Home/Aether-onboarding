ae6396c96990b835330796fe67f9fe2154e3b191
hoshikitsunoda <hoshkitsunoda@gmail.com>
2023-01-22 21:10:57 -0800

    TECH-6035 - Fixed variant names to match tags

--- types/types.ts
+++ types/types.ts
@@ -12,12 +12,12 @@ export type TypographyTagNameProps =
   | "span";
 
 export type TypographyVariantProps =
-  | "heading1"
-  | "heading2"
-  | "heading3"
-  | "heading4"
-  | "heading5"
-  | "heading6"
+  | "h1"
+  | "h2"
+  | "h3"
+  | "h4"
+  | "h5"
+  | "h6"
   | "subtitle"
   | "body"
   | "body2"
