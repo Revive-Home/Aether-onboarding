183279f79cfa1500cefaa0392915743fd3803429
hoshikitsunoda <hoshkitsunoda@gmail.com>
2023-01-22 23:36:26 -0800

    TECH-6034 - Updated stories and test

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -7,10 +7,48 @@ export default {
   component: Button,
 } as ComponentMeta<typeof Button>;
 
+const leftArrow = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth={1.5}
+    stroke="currentColor"
+    className="w-6 h-6"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18"
+    />
+  </svg>
+);
+
+const rightArrow = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth={1.5}
+    stroke="currentColor"
+    className="w-6 h-6"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"
+    />
+  </svg>
+);
+
 const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
 
 export const Primary = Template.bind({});
 Primary.args = {
-  label: "Test",
-  disabled: false,
+  variant: "primary",
+  children: "Test Button",
+  leftIcon: leftArrow,
+  rightIcon: rightArrow,
+  onClick: () => {},
+  type: "button",
 };
--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -4,13 +4,17 @@ import { Button } from "./Button";
 
 describe("Button test", () => {
   it("renders children", () => {
-    const { getByText } = render(<Button>Click!</Button>);
+    const { getByText } = render(<Button variant="primary">Click!</Button>);
     const button = getByText("Click!");
     expect(button).toBeTruthy();
   });
 
   it("renders disabled state when disabled prop is passed", () => {
-    const { getByText } = render(<Button disabled>Disabled Button</Button>);
+    const { getByText } = render(
+      <Button disabled variant="primary">
+        Disabled Button
+      </Button>
+    );
     const button = getByText("Disabled Button");
     expect(button.closest("button")).toBeDisabled();
   });
