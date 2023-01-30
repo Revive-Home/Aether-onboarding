1bf5b85ebdb49724860f4ffcf075e3ab9a0e3691
hoshikitsunoda <hoshkitsunoda@gmail.com>
2023-01-30 01:31:31 -0800

    TECH-6231 - Deleted test Dropdown component

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import "tailwindcss/tailwind.css";
 
 export { Button } from "@/components/Button";
-export { Dropdown } from "@/components/Dropdown";
 export { Typography } from "@/components/Typography";
