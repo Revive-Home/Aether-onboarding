602ae8e9ceb707b3d71c6d766dd09b259e6c5f67
hoshikitsunoda <hoshkitsunoda@gmail.com>
2023-01-30 01:49:29 -0800

    TECH-6231 - Created Input base

---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,67 @@
+import clsx from "clsx";
+
+import inputClasses from "./Input.module.css";
+
+import { Typography } from "../Typography";
+
+export interface InputProps {
+  /** Optional Tailwind class names to override or extend the styles applied to the component. */
+  className?: string;
+}
+
+export const Input = ({ className }: InputProps) => {
+  const classes = clsx(inputClasses.baseStyle, className);
+
+  const inputLabel = (
+    <Typography
+      variant="overline-lg"
+      weight="medium"
+      as="label"
+      className="!text-sm mb-2"
+    >
+      Label
+    </Typography>
+  );
+
+  const assistiveText = (
+    <Typography
+      variant="body2"
+      weight="default"
+      className="mt-1 pl-4 text-gray-400"
+    >
+      Assistive text
+    </Typography>
+  );
+
+  const infoIcon = (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      className="w-6 h-6"
+    >
+      <path
+        fillRule="evenodd"
+        d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm8.706-1.442c1.146-.573 2.437.463 2.126 1.706l-.709 2.836.042-.02a.75.75 0 01.67 1.34l-.04.022c-1.147.573-2.438-.463-2.127-1.706l.71-2.836-.042.02a.75.75 0 11-.671-1.34l.041-.022zM12 9a.75.75 0 100-1.5.75.75 0 000 1.5z"
+        clipRule="evenodd"
+      />
+    </svg>
+  );
+
+  const customInput = (
+    <div className="w-full relative">
+      <input className={classes} placeholder="Input text" type="text" />
+      <span className="suffix absolute top-1/2 right-0 transform -translate-x-1/2 -translate-y-1/2 text-darkPurple-500">
+        {infoIcon}
+      </span>
+    </div>
+  );
+
+  return (
+    <div className="flex flex-col">
+      {inputLabel}
+      {customInput}
+      {assistiveText}
+    </div>
+  );
+};
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import "tailwindcss/tailwind.css";
 
 export { Button } from "@/components/Button";
+export { Input } from "@/components/Input";
 export { Typography } from "@/components/Typography";
